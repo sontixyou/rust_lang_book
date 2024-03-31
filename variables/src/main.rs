9aa5801515a141982e174b80389511a5a7606825
kenken <kengoviolinphoto@gmail.com>
2024-03-31 16:09:44 +0900

    Add variables

---
+++ variables/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut x = 5;
+    println!("The value of x is: {}", x);
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    const MAX_POINTS: i32 = 100_000;
+
+    println!("The value MAX_POINTS is: {}", MAX_POINTS);
+}
