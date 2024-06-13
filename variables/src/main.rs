2592cd50b491cc6fb5b95e014fbab830c9c2c807
kenken <kengoviolinphoto@gmail.com>
2024-06-14 08:19:18 +0900

    shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,8 +1,18 @@
 fn main() {
+    // これはShadowingではない
     let mut x = 5;
     println!("The value of x is: {}", x);
     x = 6;
     println!("The value of x is: {}", x);
+
+    // これはShadowing
+    let y = 5;
+    let y = y + 1;
+    {
+        let y = y * 2;
+        println!("The value of y is: {}", y);
+    }
+    println!("The value of y is: {}", y);
 
     const MAX_POINTS: i32 = 100_000;
 
