eebe809794a125961b49732cbbd7d41e0b6fd221
kenken <kengoviolinphoto@gmail.com>
2024-06-14 08:37:00 +0900

    add taple and array

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -20,5 +20,31 @@ fn main() {
 
     // Data type
     // 型を指定しないとエラーになる。型の推論が出来ないため。
-    let guess = "42".parse().expect("Not a number!");
+    let guess: u32 = "42".parse().expect("Not a number!");
+
+    // Taple
+    // 一つのタプルを複数に分解することをdestructuringという
+    // 各要素の型は異なっていても良い
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of y is: {}", y);
+
+    // Array
+    // 配列の要素は全て同じ型でなければならない
+    // 要素数は固定である
+    let months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+    let a = [3; 5];
 }
