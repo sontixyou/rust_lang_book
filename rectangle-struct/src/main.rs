df7a573238ff78d9cf7ea454529af83b06e1eb1e
kenken <kengoviolinphoto@gmail.com>
2024-06-18 09:26:32 +0900

    Add struct methods

--- rectangle-struct/src/main.rs
+++ rectangle-struct/src/main.rs
@@ -46,13 +46,58 @@
 //     );
 // }
 
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
-}
+// fn area(rectangle: &Rectangle) -> u32 {
+//     rectangle.width * rectangle.height
+// }
+//
+// // rect:? is displying debug information. this statement is needed to load the Debug trait.
+// // rect:#? is pretty debug print.
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+//
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+//
+//     println!("rect1 is {rect1:#?}");
+// }
+//
+//
+// methoods are similar to functions, but they are defined within the context of a struct (or an
+// enum or a trait object).
+// Methods must have a parameter named self of type Self for their first parameter
+// implentation block looks like ruby class definition.
+// #[derive(Debug)]
+// struct Rectangle {
+//     width: u32,
+//     height: u32,
+// }
+//
+// impl Rectangle {
+//     fn area(&self) -> u32 {
+//         self.width * self.height
+//     }
+// }
+//
+// fn main() {
+//     let rect1 = Rectangle {
+//         width: 30,
+//         height: 50,
+//     };
+//
+//     println!(
+//         "The area of the rectangle is {} square pixels.",
+//         rect1.area()
+//     );
+// }
 
-// rect:? is displying debug information. this statement is needed to load the Debug trait.
-// rect:#? is pretty debug print.
-#[derive(Debug)]
+// let's try to implement multiple methods.
+
 struct Rectangle {
     width: u32,
     height: u32,
@@ -63,6 +108,24 @@ fn main() {
         width: 30,
         height: 50,
     };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
 
-    println!("rect1 is {rect1:#?}");
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+}
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
